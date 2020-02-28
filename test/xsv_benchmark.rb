0e5e4154190603b4a9ab22030383c4ab8b043556
Martijn Storck <martijn@storck.io>
2020-02-28 11:59:15 +0100

    Add a hash iteration benchmark

--- test/xsv_benchmark.rb
+++ test/xsv_benchmark.rb
@@ -4,9 +4,22 @@ require "minitest/benchmark"
 class XsvBenchmark < Minitest::Benchmark
   def bench_row_access
     zip = File.read("test/files/10k-sheet.xlsx")
+    x = Xsv::Workbook.open(zip)
     assert_performance_linear do |n|
-      x = Xsv::Workbook.open(zip)
       x.sheets[0][n]
+    end
+  end
+
+  def bench_iterate_hash
+    zip = File.read("test/files/10k-sheet.xlsx")
+    x = Xsv::Workbook.open(zip)
+    x.sheets[0].parse_headers!
+
+    assert_performance_linear do |n|
+      x.sheets[0].each_with_index do |row, i|
+        row["A1"]
+        break if i == n
+      end
     end
   end
 end
